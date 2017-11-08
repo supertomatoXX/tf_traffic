884841054848eb1eafaeb7d4a0d7b2b420410931
ckx0042 <ckx0042@175game.com>
2017-11-08 15:43:13 +0800

    new

--- train.py
+++ train.py
@@ -2,6 +2,8 @@
 # Import `tensorflow`
 import tensorflow as tf
 from load import*
+import random
+import matplotlib.pyplot as plt
 
 # Initialize placeholders
 x = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28])
@@ -18,8 +20,12 @@ logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
 
 # Define a loss function
 #定义损失函数了。sparse_softmax_cross_entropy_with_logits()，其可以计算 logits 和标签之间的稀疏 softmax 交叉熵。回归(regression)被用于预测连续值，而分类(classification)则被用于预测离散值或数据点的类别。你可以使用 reduce_mean() 来包裹这个函数，它可以计算一个张量的维度上各个元素的均值。
-loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,
-                                                                     logits=logits))
+#loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,
+#                                                                     logits=logits))
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,
+                                                                     labels=y))
+
+
 # Define an optimizer
 train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
 
@@ -43,8 +49,33 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 for i in range(201):
-    print('EPOCH', i)
-    _, accuracy_val = sess.run([train_op, accuracy], feed_dict={x: images28, y: labels})
+    #print('EPOCH', i)
+    _, accuracy_val = sess.run([train_op, loss], feed_dict={x: images28, y: labels})
     if i % 10 == 0:
-        print("Loss: ", loss)
-    print('DONE WITH EPOCH')
+        print("Loss: ", accuracy_val)
+    #print('DONE WITH EPOCH')
+
+
+# Pick 10 random images
+sample_indexes = random.sample(range(len(images28)), 10)
+sample_images = [images28[i] for i in sample_indexes]
+sample_labels = [labels[i] for i in sample_indexes]
+# Run the "predicted_labels" op.
+predicted = sess.run(correct_pred,
+		                        {x: sample_images})
+print(sample_labels)
+print(predicted)
+
+# Display the predictions and the ground truth visually.
+fig = plt.figure(figsize=(10, 10))
+for i in range(len(sample_images)):
+    truth = sample_labels[i]
+    prediction = predicted[i]
+    plt.subplot(5, 2, 1 + i)
+    plt.axis('off')
+    color = 'green' if truth == prediction else 'red'
+    plt.text(40, 10, "Truth:        {0}\nPrediction: {1}".format(truth, prediction),
+             fontsize=12, color=color)
+    plt.imshow(sample_images[i], cmap="gray")
+
+plt.show()
