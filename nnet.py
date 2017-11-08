00de5d050eb7b24d6f8503cf3583a2ec549c22a5
ckx0042 <ckx0042@175game.com>
2017-11-08 15:43:07 +0800

    new

--- nnet.py
+++ nnet.py
@@ -43,8 +43,6 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 for i in range(201):
-    print('EPOCH', i)
-    _, accuracy_val = sess.run([train_op, accuracy], feed_dict={x: images28, y: labels})
+    _, accuracy_val = sess.run([train_op, loss], feed_dict={x: images28, y: labels})
     if i % 10 == 0:
-        print("Loss: ", loss)
-    print('DONE WITH EPOCH')
+        print("Loss: ", accuracy_val)
